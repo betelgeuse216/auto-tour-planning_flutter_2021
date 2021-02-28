fc894715b9c7a6bfd650c8accabafaef4b55d6fb
ピアースギリングハム <thatguy312@live.com>
2021-03-01 08:29:53 +0900

    Implemented another responsive layout method. Updated plugin versions,.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:adobe_xd/pinned.dart';
 import 'package:adobe_xd/page_link.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:auto_tour_planning_flutter/PAGE01.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,6 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(color: Color(0xFFF5F5F5))),
       title: 'Flutter Google Maps Demo',
       home: PAGE01State(),
     );
