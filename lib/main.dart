39e9b707d03d1f5ec890957e86dd462c556a36a5
ピアースギリングハム <thatguy312@live.com>
2021-03-01 08:36:50 +0900

    Renamed page name, change link.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:auto_tour_planning_flutter/TopPage.dart';
 import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
 import 'package:adobe_xd/page_link.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:auto_tour_planning_flutter/PAGE01.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 void main() => runApp(MyApp());
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           ],
           background: Container(color: Color(0xFFF5F5F5))),
       title: 'Flutter Google Maps Demo',
-      home: PAGE01State(),
+      home: TopPage(),
     );
   }
 }
